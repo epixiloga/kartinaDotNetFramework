3677c6249093a06c08ba5835540837d71d8266e6
epixiloga <hafbenmoussa@gmail.com>
2018-12-17 17:05:19 +0100

    firstDotNetFramework

---
+++ Kartina/Controllers/VendeurController.cs
@@ -0,0 +1,33 @@
+﻿using Kartina.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kartina.Controllers
+{
+    public class VendeurController : Controller
+    {
+        // GET: Vendeur
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            Vendeur vendeur = new Vendeur();
+            using (Models.KartinaDotNetFrameworkEntities1 context=new KartinaDotNetFrameworkEntities1())
+            {
+                //var query = from item in context.Vendeur
+                //            where item.Id == id
+                //            select item;
+
+                vendeur = context.Vendeur.First(item => item.Id == id);
+            }
+            this.ViewBag.MonVendeur = vendeur;
+            return View();
+        }
+    }
+}
